9050db8fe1c02958462fd75caf27dc6f8f31623c
KwazarsLight <butov.serhei@gmail.com>
2019-07-08 14:51:22 +0300

    Add project with done base requirements

---
+++ TextFile.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Redactor
+{
+    public enum eExtensions { txt, json, xml }
+
+    public abstract class CustomFile 
+    {
+        protected string fileName;
+        protected eExtensions fileExtension;
+        protected byte[] fileContent;
+        
+
+        protected CustomFile(string fileName, eExtensions ext)
+        {
+            this.fileName = fileName;
+            this.fileExtension = ext;
+        }
+        protected CustomFile(string fileName, eExtensions ext, byte[] FileContent)
+        {
+            this.fileName = fileName;
+            this.fileExtension = ext;
+            this.fileContent = FileContent;
+        }
+
+        public static eExtensions StringToeExtensions(string strExtension)
+        {
+            return (eExtensions)Enum.Parse(typeof(eExtensions), strExtension, true);
+        }
+
+       
+
+    }
+
+    public class TextFile : CustomFile
+    {
+
+        public string FileName { get { return fileName; } set { fileName = value; } }
+
+        public eExtensions FileExtension { get {return fileExtension;} }
+
+        public byte[] FileContent { get { return fileContent; } }
+
+        public TextFile(string fileName, eExtensions ext) : base(fileName, ext)
+        { }
+
+        public TextFile(string fileName, eExtensions ext, byte[] FileContent) : base (fileName, ext, FileContent)
+        { }
+
+        public static implicit operator TextFile(FileInfo file)
+        {
+            try
+            {
+                TextFile textFile = new TextFile(file.Name, StringToeExtensions(file.Extension));
+                return textFile;
+            }
+            catch (Exception ex) { return null; }
+        }
+
+
+    }
+
+
+    public sealed class JsonFile : TextFile
+    {
+        public JsonFile(string fileName, eExtensions ext) : base(fileName, ext)
+        { }
+
+        public JsonFile(string fileName, eExtensions ext, byte[] FileContent) : base(fileName, ext, FileContent)
+        { }
+    }
+}
---
+++ Utils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Redactor
+{
+    public static class Utils
+    {
+        public static byte[] StringtToByteArray(string str, Encoding encoding)
+        {
+            return encoding.GetBytes(str);
+        }
+
+        public static String BinaryToString(byte[] data, Encoding encoder)
+        {
+            return encoder.GetString(data);
+        }
+    }
+}
